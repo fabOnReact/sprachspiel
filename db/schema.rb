efa87ef67acd24c31b72ba467535b26594433a22
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-06-02 18:45:59 +0200

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602091830) do
+ActiveRecord::Schema.define(version: 20170602164349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "buldings", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "price_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.text     "content"
