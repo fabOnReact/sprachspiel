dc9f8c21012cc52f1d8c8671e31fbb9fca3d8346
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-03-14 16:52:39 +0100

    setting db and migrating

--- db/schema.rb
+++ db/schema.rb
@@ -173,12 +173,6 @@ ActiveRecord::Schema.define(version: 20180108163350) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "timers", id: :bigserial, force: :cascade do |t|
-    t.integer  "minutes"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
