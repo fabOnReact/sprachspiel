759f8969b2a13322f232a1b635bb07b7329a71e9
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-03-14 16:15:50 +0100

    refactoring user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-   # returns the list of resource available
+   ASSETS = ["coins","gold", "wood", "wheat", "stone", "iron"]
+   EQUITY = ["equity"]
    RESOURCES = ["gold", "wood", "food", "stone", "metal"]
-   STARTING_BALANCE = [100, 100, 100, 100, 100]
+   STARTING_BALANCE = [100, 100, 100, 100, 100, 100]
+   STARTING_QUANTITIES = [100, 100, 100, 100, 100, 100]
    # Include default devise modules. Others available are:
    # :confirmable, :lockable, :timeoutable and :omniauthable
    devise :database_authenticatable, :registerable,
@@ -12,6 +14,8 @@ class User < ApplicationRecord
    has_many :purchases, :dependent => :destroy
    has_many :items, :dependent => :destroy
    has_many :sales, :dependent => :destroy
+   has_many :accounts, class_name: "Plutus::Account", foreign_key: "tenant_id"
+   # has_many :accounts, class_name: "Account", foreign_key: "tenant_id"
    belongs_to :role
 
    # accepts_nested_attributes_for :rooms
@@ -21,25 +25,54 @@ class User < ApplicationRecord
    validates :username, uniqueness: true
    validates :username, :role, presence: true
 
-   after_create :create_items
+   before_save :create_accounts
+   after_create :create_entries
 
    # def create_room
    #    @room = Room.new(title: title, description: text, building_id: self.role.building.id, user_id: self.id)        
    # end  
 
-   def create_items
-      # @purchase = Purchase.create(price_id: Price.free, room_id: self.id, selfmade: true, user_id: self.user.id)
-      # binding.pry
-      self.role.building.products.where(bonus: true).each do |product|
-         2.times do 
-            @item = Item.create(sold: false, used: false, selfmade: true, product: product, user: self)
-         end
-      end  
-      # sale = Sale.create(purchase_id: @purchase.id, room_id: self.id, price_id: self.price.id, user_id: self.user.id)    
-      # sale.purchase.items.each do |item| 
-         # item.update_attributes(room_id: purchase.room_id)
-      # end
-   end     
+   # def create_accounts
+   #    # Accounting Features from Plutus Gem
+   #    # For every user I create accounts of Assets and Liabilities to record transactions
+   #    ASSETS.map { |account| self.accounts << Plutus::Asset.create(:name => account) }
+   #    EQUITY.map { |account| self.accounts << Plutus::Equity.create(:name => account) }
+   # end   
+
+   def total_amount
+      # total amount of starting resources for us
+      STARTING_BALANCE.inject(0, :+)
+   end
+
+   def equity_id
+      self.accounts.equity.id
+   end
+
+   # def create_entries
+   #    # Every new user will have a starting amount for every account   
+   #    # I will Set them with Equity and an equal amount of Assets       
+   #    # registering the entries
+   #    entry = Plutus::Entry.new(:description => "User Creation - Initial Balance", :date => Date.today)
+      
+   #    # debiting of multiple assets accounts ["coins","gold", "wood", "wheat", "stone", "iron"]
+   #    self.accounts.assets.map.with_index do |asset, index|
+   #       entry.debit_amounts << Plutus::DebitAmount.new(:account_id => asset.id, :amount => STARTING_BALANCE[index], :quantity => STARTING_QUANTITIES[index])
+   #    end
+
+   #    # crediting of one equity account
+   #    entry.credit_amounts << Plutus::CreditAmount.new(:account_id => equity_id, :amount => total_amount)
+   #    entry.save
+      
+   #    # self.role.building.products.where(bonus: true).each do |product|
+   #    #    2.times do 
+   #    #       @item = Item.create(sold: false, used: false, selfmade: true, product: product, user: self)
+   #    #    end
+   #    # end  
+   #    # sale = Sale.create(purchase_id: @purchase.id, room_id: self.id, price_id: self.price.id, user_id: self.user.id)    
+   #    # sale.purchase.items.each do |item| 
+   #       # item.update_attributes(room_id: purchase.room_id)
+   #    # end
+   # end     
 
 
    # splits the email and uses the part before the @ for the name
@@ -91,23 +124,23 @@ class User < ApplicationRecord
       total_sales.calculation(substract_array, total_purchases)
    end
 
-   def user_has_room(building)
-      if self.admin 
-         return false
-      elsif building.id == 4 
-         return true
-      else
-         return self.present? && self.rooms.present? 
-      end
-   end
+   # def user_has_room(building)
+   #    if self.admin 
+   #       return false
+   #    elsif building.id == 4 
+   #       return true
+   #    else
+   #       return self.present? && self.rooms.present? 
+   #    end
+   # end
 
    def clear_purchases(room_id)
       self.purchases.where(room_id: room_id, sale_id: nil, selfmade: nil).destroy_all
    end
 
-   def room_owner(room)
-      self == room.user
-   end
+   # def room_owner(room)
+   #    self == room.user
+   # end
 
    def admin?
       self.admin == true
