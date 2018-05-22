9de43f227ea18f6548dad5dbacc610c69fb47e62
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-05-22 23:00:27 +0200

    updating links for events#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	devise_for :users, controllers: {registrations: 'users/registrations'}
-	resources :messages, :items, :products, :purchases, :chatrooms, :events
+	resources :messages, :items, :products, :purchases, :chatrooms, :events, :alliances
 	resource :subscriptions, only: [:new, :create]
 	# ajax action to add items to form
 	post "purchases/:product_id", to: "purchases#new", as: "add_item"
