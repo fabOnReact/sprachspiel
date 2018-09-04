4c15b4296576763931cf3ce304ad64f4be0da67b
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-09-04 15:50:55 +0800

    user join existing alliance

--- app/controllers/alliances_controller.rb
+++ app/controllers/alliances_controller.rb
@@ -7,6 +7,18 @@ class AlliancesController < ApplicationController
   def show; end
 
 	def new; @alliance = Alliance.new; end
+	
+	def update
+		@alliance = Alliance.find params[:id]
+		@alliance.users << current_user
+		if @alliance.save 
+			redirect_to :show
+		else 
+			flash[:alert] = "Impossible to join alliance"
+			render :index
+		end
+	end
+
   # private
   # def alliance_params
   #   params.require(:alliance).permit(:name, :description, user_ids: [])
