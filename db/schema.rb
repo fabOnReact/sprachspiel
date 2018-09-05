8f0e8dbbec0bf22160d01d48ec9e1adb271518fd
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-09-05 13:45:44 +0800

    association between chatroom and alliance

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180611184819) do
+ActiveRecord::Schema.define(version: 20180905053934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "alliances", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "chatroom_id"
+    t.index ["chatroom_id"], name: "index_alliances_on_chatroom_id", using: :btree
   end
 
   create_table "buildings", force: :cascade do |t|
@@ -220,6 +222,7 @@ ActiveRecord::Schema.define(version: 20180611184819) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "alliances", "chatrooms"
   add_foreign_key "categories", "events"
   add_foreign_key "descriptions", "products"
   add_foreign_key "invites", "alliances"
