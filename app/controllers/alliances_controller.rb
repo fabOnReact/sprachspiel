d5b1129fe5cf1556629c62a1860bc44aee2255e6
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-09-14 14:15:43 +0800

    refactoring alliance/event controller/models

--- app/controllers/alliances_controller.rb
+++ app/controllers/alliances_controller.rb
@@ -1,8 +1,8 @@
 class AlliancesController < ApplicationController
-	before_filter :find_alliance, on: [:show, :update] 
-	before_filter :create_alliance, on: [:create]
-	before_filter :add_user, on: [:create, :update]
-	after_filter :save_and_redirect, on: [:create, :update]
+	before_action :find_alliance, only: [:show, :update] 
+	before_action :create_alliance, only: [:create]
+	before_action :add_user, only: [:create, :update]
+	# after_filter :save_and_redirect, only: [:create, :update]
   def index
     @alliances = Alliance.all
     @alliance = Alliance.new
@@ -10,23 +10,27 @@ class AlliancesController < ApplicationController
 
   def show; @message = Message.new; end
 	def new; @alliance = Alliance.new; end
-	def create; end
+	def create;
+  	save_and_redirect
+	end
 	def update; end
 
   private
-	def create_user; @alliance = Alliance.new alliance_params; end
+	def create_alliance;
+	 	@alliance = Alliance.new alliance_params; end
 	def add_user; @alliance.users << current_user; end
-	def find_user; @alliance = Alliance.find params[:id]; end 
-	
-  def save_and_redirect
-		if @alliance.save; redirect_to :show;
-	 	else; 
-			flash[:alert] = "Impossible to create alliance"
-			render :index
-		end
-	end
+	def find_alliance; @alliance = Alliance.find params[:id]; end 
 
   def alliance_params
     params.require(:alliance).permit(:name, :description, user_ids: [])
   end  
+
+  def save_and_redirect
+		if @alliance.save 
+			redirect_to alliance_path(@alliance)
+	 	else 
+			flash[:alert] = "Impossible to save alliance"
+			render :index
+		end
+	end
 end
--- app/models/alliance.rb
+++ app/models/alliance.rb
@@ -1,6 +1,5 @@
 class Alliance < Event
 	after_create :add_chatroom
-  before_save :add_user
   self.table_name = "events"
   HEADERS = %w[name description participants join]
   has_many :invites
