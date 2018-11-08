f9155edd1a775514ba9956699f256cac457a71f4
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-11-08 14:02:58 +0100

    rewriting seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,42 @@
 # Creating products and properties with the images present in the pipeline
-products = {"sichel" => [:Energie, 10],"hammer" => [:Energie, 20],"achse" => [:Angriff, 20],"messer" => [:Angriff, 10],"fisch" => [:Gesundheit, 20],"bank" => [:Energie, 30],"ofen" => [:Energie, 10],"hähnchen" => [:Energie, 20],"schwert" => [:Angriff, 35],"schild" => [:Verteidigung, 20],"brot"=> [:Energie, 30]}
-products.keys.map do |key|
-	property = Property.find_or_create_by(name: products[key][0], amount: products[key][1] )
-	product = Product.find_or_create_by(name: key, property: property)
-    puts "product #{product.name} has the following errors #{product.errors.full_messages}" if product.errors.present?
+# attributes for properties
+properties = [{name: :Energie, amount: 10}, {name: :Energie, amount: 20}, {name: :Angriff, amount: 20}, {name: :Angriff, amount: 10}, {name: :Gesundheit, amount: 20}, {name: :Energie, amount: 30}, {name: :Angriff, amount: 35},  {name: :Verteidigung, amount: 20}]
+# attributes for products
+products = [{name: :sichel, price: 10, property_id: 1},{name: :hammer, price: 20, property_id: 2}, {name: :achse, price: 30, property_id: 3}, {name: :messer, price: 40, property_id: 4}, {name: :fisch, price: 10, property_id: 5}, {name: :bank, price: 15, property_id: 6}, {name: :ofen, price: 15, property_id: 1}, {name: :hähnchen, price: 10, property_id: 2}, {name: :schwert, price: 40, property_id: 7}, {name: :schild, price: 50, property_id: 8}, {name: :brot, price: 10, property_id: 6}  ]
+puts "1 ----- creating properties"
+properties.each_with_index do |attributes, index|
+  property = Property.find_or_create_by attributes
+  LogMessage.new property, index
 end
-# Creating Roles 
+   
+puts "2 ----- creatings products"
+products.each_with_index do |attributes, index|
+  product = Product.find_or_create_by attributes
+  LogMessage.new product, index
+end
+
+puts "3 ----- creating roles"
 farmer = Role.create(name: "Farmer")
-Role.create(name: "Schmied")
+schmied = Role.create(name: "Schmied")
 soldier = Role.create(name: "Soldat")
-Role.create(name: "König")
-# Creating User
+konig = Role.create(name: "König")
+LogMessage.new farmer, 1
+LogMessage.new schmied, 2
+LogMessage.new soldier, 2
+LogMessage.new konig, 2
+
+puts "4 ----- creating users"
 user1 = User.create(email: 'fabrizio@email.com', password: 'fabrizio', username: 'fabrizio', role: soldier)
-puts "user1 errors #{user1.errors.full_messages}"
+LogMessage.new user1, 1 
 user2 = User.create(email: 'ezio@email.com', password: 'fabrizio', username: 'ezio', role: farmer)
-# Creating some basic messages
+LogMessage.new user2, 2
+
+puts "5 ----- creating chatroom messages"
 contents = ["Hello, welcome to our new application", "Here you can exercise your german speaking skills with other students", "The game is fun!", "Many new feature will come out soon!"]
 chatroom = Chatroom.create()
+LogMessage.new chatroom, 1
 contents.each_with_index do |content, index|
   user = index.odd? ? user1 : user2
   message = Message.create(user: user, chatroom: chatroom, content: content)
-  puts "the messages errors are #{message.errors.full_messages}" if message.errors.present?
-  puts "the user is #{user.inspect}"
+  LogMessage.new message, index
 end
