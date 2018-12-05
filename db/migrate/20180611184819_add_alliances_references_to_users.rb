2fe1a982d1bf1dae843130bd5b359c22a3360869
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-12-05 20:22:48 +0100

    remove alliance reference to users

--- db/migrate/20180611184819_add_alliances_references_to_users.rb
+++ db/migrate/20180611184819_add_alliances_references_to_users.rb
@@ -1,5 +1,5 @@
 class AddAlliancesReferencesToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_reference :users, :alliance, foreign_key: true
+    # add_reference :users, :alliance, foreign_key: true
   end
 end
