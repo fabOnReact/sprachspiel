5fa68d9b212b71b676e1414751e1cb6c2cc9ba2d
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-12-05 20:20:59 +0100

    modifing invites dg migration

--- db/migrate/20180610112746_create_invites.rb
+++ db/migrate/20180610112746_create_invites.rb
@@ -2,7 +2,7 @@ class CreateInvites < ActiveRecord::Migration[5.0]
   def change
     create_table :invites do |t|
       t.references :user, foreign_key: true
-      t.references :alliance, foreign_key: true
+      # t.references :alliance, foreign_key: true
 
       t.timestamps
     end
