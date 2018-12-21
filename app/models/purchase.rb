f1f27e235387e953abb605c75e54cde8c13eb897
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-12-21 14:55:37 +0100

    fixing format

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,22 +1,22 @@
 class Purchase < ApplicationRecord
-	has_many :items, inverse_of: :purchase
-	belongs_to :user
-	
-	accepts_nested_attributes_for :items
-	validates :price, presence: true
+  has_many :items, inverse_of: :purchase
+  belongs_to :user
+  
+  accepts_nested_attributes_for :items
+  validates :price, presence: true
   validates :price, numericality: { greater_than: 0}
 
-	before_save :user_has_money
-	after_save :update_balance
+  before_save :user_has_money
+  after_save :update_balance
 
-	scope :price_sum, ->(resource) { joins(:price).sum(resource) }
+  scope :price_sum, ->(resource) { joins(:price).sum(resource) }
 
-	def update_balance
-		user.money -= self.price
+  def update_balance
+    user.money -= self.price
     user.update_attribute(:money, user.money)
   end
 
-	def user_has_money
-	  errors.add(:price, "Price is too high!") if user.money < price
-	end
+  def user_has_money
+    errors.add(:price, "Price is too high!") if user.money < price
+  end
 end
